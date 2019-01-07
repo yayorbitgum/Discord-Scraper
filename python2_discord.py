1e7164684b4f3ec62e00d54f8c5424e6e00ffa2e
Dracovian <25242161+Dracovian@users.noreply.github.com>
2019-01-07 00:57:53 -0600

    Fixed issues with script.
    Fixed same issue as seen in the Python 3 variant and the issue with the filename being spaced out by multiple period characters.

--- python2_discord.py
+++ python2_discord.py
@@ -104,9 +104,9 @@ class Discord:
                     file_data.append(attachment['url'])
 
         # Download the files.
-        for url in new_data:
+        for url in file_data:
             filename = '{}_{}'.format(url.split('/')[-2], url.split('/')[-1])
-            file_location = path.join(getcwd(), 'Discord Scrapes', server_id, channel_id, '{0}'.format('.'.join(filename))
+            file_location = path.join(getcwd(), 'Discord Scrapes', server_id, channel_id, '{0}'.format(filename))
             self.grabFile(file_location, url)
 
     '''
