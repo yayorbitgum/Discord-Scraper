a4ee8161772c9e054b8392bf9fff1f89d1ef662a
Dracovian <25242161+Dracovian@users.noreply.github.com>
2019-02-12 02:47:42 -0600

    Bugfix Attempt #1
    Fixed the text gathering function so that it can be imported into a database since CSVs are well known and supported.
    
    This is labelled as an attempt since there were some development issues along the way with this release; so don't hold back on the issues because I can't test all possible combinations of systems and servers by myself.
    
    I also added some documentation again to the file and I improved the separation of the web scraping classes between the Python 2.7 and Python 3 interpreter support.

--- discord.py
+++ discord.py
@@ -1,191 +1,206 @@
+"""
+The modules (imports)
+
+The os module will be used to create directories and delete files.
+The sys module will be used to output text without being messed up by threading.
+The json module will be used to convert a JSON (JavaScript Object Notation) string to a Python dictionary.
+The unicodedata module will be used to handle those nasty UCS2 encoded characters that don't play well with UTF/ISO-8859-1 encoded files.
+The threading module will be used to run this script's functions on multiple software threads (increasing system resource usage for faster runtime).
+The random module will be used to generate the random alphanumeric values by the random_string lambda function.
+"""
 from os import path, makedirs, getcwd, remove
 from sys import stderr, stdout, version_info
 from json import loads as json_to_array
+from unicodedata import normalize
 from threading import Thread
 from random import choice
-global is_py3, charset
 
-charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwyxz0123456789'
-random_string = lambda length: ''.join([choice(charset) for i in range(length)])
+"""
+The global variable
 
+textlink will be an array/list to keep track of the current page of textual contents since threading is screwy (appending duplicates ad infinitum making for excessively large filesizes when grabbing channel text).
+imagetypes will be an array/list to keep track of the allowed image file extensions (again the future mimetype implementation will make this obsolete).
+videotypes will be an array/list to keep track of the allowed video file extensions (again the future mimetype implementation will make this obsolete).
+"""
+textlink = ['id,server_id,channel_id,timestamp,author_id,author_name,content,embeds']
+
+imagetypes = [
+    'jpg', 'jpeg', 'png', 'gif',
+    'tiff', 'tif', 'bmp', 'tga',
+    'webp', 'apng'
+]
+
+videotypes = [
+    'ogv', 'webm', 'mp4', 'mp2',
+    'mpeg', 'mkv', 'bnk', 'avi',
+    'h264', 'h265', 'wmv', 'raw',
+    'vob', 'mpg'
+]
+
+"""
+The anonymous functions (lambda functions)
+
+random_string(length):  This function should return a random alphanumeric value of length 'length' when called.
+fix_utf_errors(string): This function should return a string that is safe to write in an ISO-8859-1 encoded file.
+py3_url_split(url):     This function should return a single-dimensional dictionary which is used by http.client to make server requests.
+"""
+random_string = lambda length: ''.join([choice('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789') for i in range(length)])
+fix_utf_errors = lambda string: normalize('NFKC', string).encode('iso-8859-1', 'ignore').decode('iso-8859-1')
+py3_url_split = lambda url: [url.split('/')[2], '/{0}'.format('/'.join(url.split('/')[3::]))]
+
+"""
+Website Request class
+
+This portion of the script is what allows us to make web requests without
+needing to worry about creating separate scripts for Python 2.7 and Python 3
+compatibility like I did in the past commits.
+
+You can simply reuse this portion of the script if you were wanting to make
+a web scraper that works with Python 2.7 and Python 3 interpreters. I don't
+have any interest in supporting older versions of Python 2 and if we ever
+get a Python 4 release, then I'll be sure to make updates to this portion
+of the script.
+"""
 if version_info[0] == 3:
     from http.client import HTTPSConnection
-    is_py3 = True
-elif version_info[0] == 2:
+
+    class Request:
+        def __init__(self, headers):
+            self.headers = headers
+
+        def grab_page(self, url, binary=False, error='Received HTTP {0} error when gathering JSON data.\n'):
+            domain, path = py3_url_split(url)
+            conn = HTTPSConnection(domain, 443)
+            conn.request('GET', path, headers=self.headers)
+
+            resp = conn.getresponse()
+            if resp.status == 200:
+                return resp.read() if binary else json_to_array(resp.read())
+            else:
+                stderr.write(error.format(resp.status))
+                stdout.write(resp.read().decode('iso-8859-1'))
+                return ''
+
+
+elif version_info[0] == 2 and version_info[1] >= 7:
     from urllib2 import build_opener, install_opener, urlopen, HTTPError
-    is_py3 = False
+
+    class Request:
+        def __init__(self, headers):
+            self.headers = headers
+
+        def grab_page(self, url, binary=False, error='Received HTTP {0} error when gathering JSON data.\n'):
+            opener_headers = []
+            for k, v in zip(self.headers.keys(), self.headers.values()):
+                opener_headers.append((k, v.encode('iso-8859-1')))
+
+            opener = build_opener()
+            opener.addheaders = opener_headers
+            install_opener(opener)
+
+            try:
+                return urlopen(url).read() if binary else json_to_array(urlopen(url).read())
+            except HTTPError as err:
+                stderr.write(error.format(err.code))
+                return ''
+
 else:
     stderr.write('Unsupported version of Python detected!')
     exit(1)
 
-class DiscordRequest:
-    def __init__(self, url, headers, types):
-        self.domain = url.split('/')[2]
-        self.path = '/{0}'.format('/'.join(url.split('/')[3::]))
-        self.headers = headers
+"""
+Discord Scraper class
 
-        self.channel = ''
-        self.server = ''
-        self.types = types.replace(' ', '').split(',')
+This class will serve the purpose of grabbing data from Discord's servers.
+You can still make requests to other web servers without this class.
 
-        self.imagetypes = [
-            'jpg', 'jpeg', 'png', 'gif',
-            'tiff', 'tif', 'bmp', 'tga',
-            'webp', 'apng'
-        ]
+If you were to make updates to this script to work with future versions of
+the Discord API, this would be the class you need to modify.
 
-        self.videotypes = [
-            'ogv', 'webm', 'mp4', 'mp2',
-            'mpeg', 'mkv', 'bnk', 'avi',
-            'h264', 'h265', 'wmv', 'raw',
-            'vob', 'mpg'
-        ]
+The current API version of Discord from the time of this script: v6
+"""
 
-    def download_file(self, file, filename):
-        filerr = 0
+class DiscordScraper:
+    def __init__(self):
+        with open('discord.json', 'r') as config_file:
+            config = json_to_array(config_file.read())
 
-        with open(filename, 'wb') as binaryfile:
-            if is_py3:
-                conn = HTTPSConnection(file.split('/')[2], 443)
-                conn.request('GET', '/{0}'.format('/'.join(file.split('/')[3::])))
-                resp = conn.getresponse()
+        self.pages = config['pages']
+        self.query = config['query']
+        self.types = config['types'].replace(' ', '').split(',')
+        self.header = {'user-agent': config['agent'], 'authorization': config['token']}
+        self.servers = config['servers']
 
-                if resp.status != 200:
-                    filerr = 1
-                    stderr.write('Failed to grab binary data for \'{0}\' with an HTTP {1} error.\n'.format(filename, resp.status))
-                else:
-                    binaryfile.write(resp.read())
-            else:
-                opener_headers = []
-                for k, v in zip(self.headers.keys(), self.headers.values()):
-                    opener_headers.append((k, v.encode('iso-8859-1')))
+    def get_server_name_by_id(self, server_id):
+        request = Request(self.header)
+        server_data = request.grab_page('https://discordapp.com/api/v6/guilds/{0}'.format(server_id))
 
-                opener = build_opener()
-                opener.addheaders = opener_headers
-                install_opener(opener)
-
-                try:
-                    binaryfile.write(urlopen(file).read())
-                except HTTPError as httperr:
-                    filerr = 1
-                    stderr.write('Failed to grab binary data for \'{0}\' with an HTTP {1} error.\n'.format(filename, httperr.code))
-
-        if filerr == 1:
-            remove(filename)
-            filerr = 0
-
-    def get_name_from_id(self, topic, id):
-        if is_py3:
-            conn = HTTPSConnection(self.domain, 443)
-            conn.request('GET', '/api/v6/{0}/{1}'.format(topic, id), headers=self.headers)
-            resp = conn.getresponse()
-
-            if resp.status != 200:
-                stderr.write('Received HTTP {0} error when gathering JSON data.\n'.format(resp.status))
-                contents = []
-            else:
-                contents = json_to_array(resp.read())
+        if len(server_data) > 0:
+            return server_data['name']
         else:
-            opener_headers = []
-            for k, v in zip(self.headers.keys(), self.headers.values()):
-                opener_headers.append((k, v.encode('iso-8859-1')))
+            random_name = random_string(12)
+            stderr.write('Unable to fetch server name from id, so we\'ll be using {0} instead.'.format(random_name))
+            return random_name
 
-            opener = build_opener()
-            opener.addheaders = opener_headers
-            install_opener(opener)
+    def get_channel_name_by_id(self, channel_id):
+        request = Request(self.header)
+        channel_data = request.grab_page('https://discordapp.com/api/v6/channels/{0}'.format(channel_id))
 
-            try:
-                contents = json_to_array(urlopen('https://{0}/api/v6/{1}/{2}'.format(self.domain, topic, id)).read())
-            except HTTPError as httperr:
-                stderr.write('Received HTTP {0} error when gathering JSON data.\n'.format(httperr.code))
-                contents = []
+        if len(channel_data) > 0:
+            return channel_data['name']
+        else:
+            random_name = random_string(12)
+            stderr.write('Unable to fetch channel name from id, so we\'ll be using {0} instead.'.format(random_name))
+            return random_name
 
-        if len(contents) > 0:
-            if topic == 'guilds':
-                self.server = contents['name']
-            elif topic == 'channels':
-                self.channel = contents['name']
-            else:
-                stderr.write('Invalid topic \'{0}\' please use either \'guilds\' or \'channels\' for the topic.\n'.format(topic))
+    def create_folders(self, server_id, channel_id):
+        if not path.exists(path.join(getcwd(), 'Discord Scrapes', self.get_server_name_by_id(server_id), self.get_channel_name_by_id(channel_id))):
+            makedirs(path.join(getcwd(), 'Discord Scrapes', self.get_server_name_by_id(server_id), self.get_channel_name_by_id(channel_id)))
 
-    def gather_data(self):
+        return path.join(getcwd(), 'Discord Scrapes', self.get_server_name_by_id(server_id), self.get_channel_name_by_id(channel_id))
+
+    def grab_data(self, page):
         threads = []
 
-        if self.server == '':
-            self.server = random_string(12)
-            stderr.write('Failed to gather server name from ID, defaulting to a random folder name: {0}.\n'.format(self.server))
+        for server in self.servers.keys():
+            for channels in self.servers.values():
+                for channel in channels:
+                    folder = self.create_folders(server, channel)
+                    request = Request(self.header)
+                    contents = request.grab_page('https://discordapp.com/api/v6/guilds/{0}/messages/search?channel_id={1}&offset={2}&{3}'.format(server, channel, 25 * page, self.query))
 
-        if self.channel == '':
-            self.channel = random_string(12)
-            stderr.write('Failed to gather channel name from ID, defaulting to a random folder name: {0}.\n'.format(self.channel))
+                    for messages in contents['messages']:
+                        filelink = []
 
-        filepath = path.join(getcwd(), 'Discord Scrapes', self.server, self.channel)
-        filelink = []
-        linklink = []
-        textlink = []
+                        for message in messages:
+                            for attachment in message['attachments']:
+                                if 'image' in self.types and attachment['filename'].split('.')[-1] in imagetypes:
+                                    filelink.append(attachment['url'])
+                                if 'video' in self.types and attachment['filename'].split('.')[-1] in videotypes:
+                                    filelink.append(attachment['url'])
+                                if 'file' in self.types and attachment['filename'].split('.')[-1] not in imagetypes and videotypes:
+                                    filelink.append(attachment['url'])
 
-        if not path.exists(filepath):
-            makedirs(filepath)
+                            if 'text' in self.types:
+                                if len(message['embeds']) > 0:
+                                    embed_urls = []
 
-        if is_py3:
-            conn = HTTPSConnection(self.domain, 443)
-            conn.request('GET', self.path, headers=self.headers)
-            resp = conn.getresponse()
+                                    for embed in message['embeds']:
+                                        embed_urls.append(embed['url'])
 
-            if resp.status != 200:
-                stderr.write('Received HTTP {0} error when gathering JSON data.\n'.format(resp.status))
-                contents = []
-            else:
-                contents = json_to_array(resp.read())
-        else:
-            opener_headers = []
-            for k, v in zip(self.headers.keys(), self.headers.values()):
-                opener_headers.append((k, v.encode('iso-8859-1')))
+                                    embed_data = ';'.join(embed_urls)
+                                else: embed_data = 'null'
 
-            opener = build_opener()
-            opener.addheaders = opener_headers
-            install_opener(opener)
+                                if(len(fix_utf_errors(message['content']).replace(',', '.').replace('\r', ' ').replace('\n', ' ')) > 0):
+                                    textdata = fix_utf_errors(message['content']).replace(',', '.').replace('\r', ' ').replace('\n', ' ')
+                                else: textdata = 'null'
 
-            try:
-                contents = json_to_array(urlopen('https://{0}{1}'.format(self.domain, self.path)).read())
-            except HTTPError as httperr:
-                stderr.write('Received HTTP {0} error when gathering JSON data.\n'.format(httperr.code))
-                contents = []
+                                object = [message['id'], server, message['channel_id'], message['timestamp'], message['author']['id'], '#'.join([fix_utf_errors(message['author']['username']), message['author']['discriminator']]), textdata, embed_data]
+                                textlink.append(','.join(object))
 
-        if len(contents) > 0:
-            for messages in contents['messages']:
-                for message in messages:
-                    if len(message['attachments']) > 0:
-                        for attachment in message['attachments']:
-                            if 'images' in self.types and attachment['filename'].split('.')[-1] in self.imagetypes:
-                                filelink.append(attachment['url'])
-
-                            if 'videos' in self.types and attachment['filename'].split('.')[-1] in self.videotypes:
-                                filelink.append(attachment['url'])
-
-                            if 'files' in self.types and attachment['filename'].split('.')[-1] not in self.imagetypes and attachment['filename'].split('.')[-1] not in self.videotypes:
-                                filelink.append(attachment['url'])
-
-                    if len(message['embeds']) > 0:
-                        for embeds in message['embeds']:
-                            if 'embeds' in self.types and embeds['type'] == 'link':
-                                linklink.append(embeds['url'])
-
-                    if message['content'] != '':
-                        if 'texts' in self.types:
-                            textlink.append(': '.join([message['author']['id'], message['content']]))
-
-
-        with open('{0}\\{1}_{2}_text.csv'.format(filepath, self.server, self.channel), 'a') as textfile:
-            textfile.write(','.join(set(textlink)))
-
-        with open('{0}\\{1}_{2}_link.csv'.format(filepath, self.server, self.channel), 'a') as textfile:
-            textfile.write(','.join(set(textlink)))
-
-        for file in set(filelink):
-            filename = '{0}\\{1}_{2}'.format(filepath, file.split('/')[-2], file.split('/')[-1])
-            t = Thread(target=self.download_file, args=(file, filename, ))
-            threads.append(t)
+                        for file in filelink:
+                            t = Thread(target=self.download_binary, args=(file, folder, ))
+                            threads.append(t)
 
         for thread in threads:
             thread.start()
@@ -193,21 +208,41 @@ class DiscordRequest:
 
         del threads[:]
 
+    def download_binary(self, url, folder):
+        request = Request({'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'})
+        filename = '{0}_{1}'.format(url.split('/')[-2], url.split('/')[-1])
+
+        binary_data = request.grab_page(url, True, 'Received HTTP {0} error when gathering binary data for \'%s\'.\n' % url)
+        if len(binary_data) > 0:
+            with open(path.join(folder, filename), 'wb') as binary:
+                binary.write(binary_data)
+
+    def download_log(self, folder):
+        if path.exists(path.join(folder, 'channels.csv')):
+            remove(path.join(folder, 'channels.csv'))
+
+        with open(path.join(folder, 'channels.csv'), 'a') as csv:
+            for text in textlink:
+                csv.write('{0}\n'.format(text))
+
+"""
+The start of this script.
+
+No matter how you decide to run this script, this portion should be the first one called.
+This makes it easier for me to sort out non-class functions from the start of the script.
+"""
 if __name__ == '__main__':
-    with open('discord.json', 'r') as config:
-        config_data = json_to_array(config.read())
+    ds = DiscordScraper()
+    threads = []
 
-    token = config_data['token']
-    pages = config_data['pages']
-    query = config_data['query']
-    agent = config_data['agent']
-    types = config_data['types']
-    servers = config_data['servers']
+    for i in range(ds.pages):
+        t = Thread(target=ds.grab_data, args=(i, ))
+        threads.append(t)
 
-    for server, channels in zip(servers.keys(), servers.values()):
-        for channel in channels:
-            for i in range(0, pages + 1):
-                dr = DiscordRequest('https://discordapp.com/api/v6/guilds/{0}/messages/search?has=image&has=video&channel_id={1}&offset={2}&include_nsfw=true'.format(server, channel, i * 25), {'User-agent': agent, 'authorization': token}, types)
-                dr.get_name_from_id('guilds', server)
-                dr.get_name_from_id('channels', channel)
-                dr.gather_data()
+    for thread in threads:
+        thread.start()
+        thread.join()
+
+    ds.download_log(path.join(getcwd(), 'Discord Scrapes'))
+    del threads[:]
+    del textlink[:]
