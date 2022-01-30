8c1e8cdbf724ec9d79929e039d2c250595004089
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2022-01-30 17:46:05 -0600

    - Refactor string formatting to f-strings. - Swap docstring imports to comments. - Refactor shadowed variable names. - Refactor long strings for readability. - Refactor equality operator for preferred "is" keyword. - Remove unnecessary os import for exit method.

--- discord.py
+++ discord.py
@@ -1,59 +1,47 @@
 """
 @author:  Dracovian
 @date:    2021-02-10
+@fork:    Yayorbitgum
+@update:  2022-01-30
 @license: WTFPL
 """
-
-"""
-datetime.timedelta: Used to subtract an entire day from the current one.
-datetime.datetime:  Used to retrieve the current day.
-"""
+# datetime.timedelta: Used to subtract an entire day from the current one.
+# datetime.datetime:  Used to retrieve the current day.
 from datetime import timedelta, datetime
-
-"""
-module.DiscordScraper: Used to access the Discord Scraper class functions.
-"""
+# module.DiscordScraper: Used to access the Discord Scraper class functions.
 from module import DiscordScraper
-
-"""
-os._exit: Used to exit the script.
-"""
-from os import _exit as exit
-
-"""
-module.DiscordScraper.loads: Used to access the json.loads function documented in the DiscordScraper class file.
-"""
+# module.DiscordScraper.loads: Used to access the json.loads function documented in the DiscordScraper class file.
 from module.DiscordScraper import loads
 
 
-def getLastMessageGuild(scraper, guild, channel):
+def getLastMessageGuild(scraper, guild_messages, guild_channel):
     """
     Use the official Discord API to retrieve the last publicly viewable message in a channel.
     :param scraper: The DiscordScraper class reference that we will be using.
-    :param guild: The ID for the guild that we're wanting to scrape from.
-    :param channel: The ID for the channel that we're wanting to scrape from.
+    :param guild_messages: The ID for the guild that we're wanting to scrape from.
+    :param guild_channel: The ID for the channel that we're wanting to scrape from.
     """
-
-    # Generate a valid URL to the documented API function for retrieving channel messages (we don't care about the 100 message limit this time).
-    lastmessage = 'https://discord.com/api/{0}/channels/{1}/messages?limit=1'.format(scraper.apiversion, channel)
-
+    # Generate a valid URL to the documented API function for retrieving channel messages
+    # (we don't care about the 100 message limit this time).
+    message_limit = 1
+    lastmessage = f'https://discord.com/api' \
+                  f'/{scraper.apiversion}' \
+                  f'/channels/{guild_channel}' \
+                  f'/messages?limit={message_limit}'
     # Update the HTTP request headers to set the referer to the current guild channel URL.
-    scraper.headers.update({'Referer': 'https://discord.com/channels/{0}/{1}'.format(guild, channel)})
+    scraper.headers.update({'Referer': f'https://discord.com/channels/{guild_messages}/{guild_channel}'})
 
     try:
         # Execute the network query to retrieve the JSON data.
         response = DiscordScraper.requestData(lastmessage, scraper.headers)
-
         # If we returned nothing then return nothing.
         if response is None:
             return None
 
         # Read the response data and convert it into a dictionary object.
         data = loads(response.read())
-
         # Retrieve the snowflake of the post and convert it into a timestamp.
         timestamp = DiscordScraper.snowflakeToTimestamp(int(data[0]['id']))
-
         # Return the datetime object from the given timestamp above.
         return datetime.fromtimestamp(timestamp)
 
@@ -90,32 +78,30 @@ def startGuild(scraper, guild, channel, day=None):
     :param guild: The ID for the guild that we're wanting to scrape from.
     :param channel: The ID for the channel that we're wanting to scrape from.
     """
-
     # Get the snowflakes for the current day.
     snowflakes = DiscordScraper.getDayBounds(day.day, day.month, day.year)
-
     # Generate a valid URL to the undocumented API function for the search feature.
-    search = 'https://discord.com/api/{0}/channels/{1}/messages/search?min_id={2}&max_id={3}&{4}'.format(
-        scraper.apiversion, channel, snowflakes[0], snowflakes[1], scraper.query)
+    search_api_url = f'https://discord.com/api/{scraper.apiversion}' \
+                     f'/channels/{channel}/messages/search' \
+                     f'?min_id={snowflakes[0]}' \
+                     f'&max_id={snowflakes[1]}' \
+                     f'&{scraper.query}'
 
     # Update the HTTP request headers to set the referer to the current guild channel URL.
-    scraper.headers.update({'Referer': 'https://discord.com/channels/{0}/{1}'.format(guild, channel)})
-
+    scraper.headers.update({'Referer': f'https://discord.com/channels/{guild}/{channel}'})
     # ----------------------------------------------------
     # Generate the guild name.
-    if scraper.guildname == None:
+    if scraper.guildname is None:
         scraper.grabGuildName(guild)
-
     # Generate the channel name.
-    if scraper.channelname == None:
+    if scraper.channelname is None:
         scraper.grabChannelName(channel)
-
     # Generate the scrape folders.
     scraper.createFolders()
 
     # ----------------------------------------------------
     # Grab the API response for the search query URL.
-    response = DiscordScraper.requestData(search, scraper.headers)
+    response = DiscordScraper.requestData(search_api_url, scraper.headers)
     # If we returned nothing then continue on to the previous day.
     if response is None:
         # Set the day to yesterday.
@@ -131,37 +117,42 @@ def startGuild(scraper, guild, channel, day=None):
 
     # ----------------------------------------------------
     # Determine if we have multiple offsets.
-    if (posts > 25):
+    if posts > 25:
         pages = int(posts / 25) + 1
-
         for page in range(2, pages + 1):
             # Generate a valid URL to the undocumented API function for the search feature.
-            search = 'https://discord.com/api/{0}/channels/{1}/messages/search?min_id={2}&max_id={3}&{4}&offset={5}'.format(
-                scraper.apiversion, channel, snowflakes[0], snowflakes[1], scraper.query, 25 * (page - 1))
+            search_api_url = f'https://discord.com/api/{scraper.apiversion}' \
+                     f'/channels/{channel}/messages/search' \
+                     f'?min_id={snowflakes[0]}' \
+                     f'&max_id={snowflakes[1]}' \
+                     f'&{scraper.query}' \
+                     f'&offset={25 * (page - 1)}'
+
             # Update the HTTP request headers to set the referer to the current guild channel URL.
-            scraper.headers.update({'Referer': 'https://discord.com/channels/{0}/{1}'.format(guild, channel)})
+            scraper.headers.update({'Referer': f'https://discord.com/channels/{guild}/{channel}'})
+
             # ----------------------------------------------------
             # Grab the API response for the search query URL.
-            response = DiscordScraper.requestData(search, scraper.headers)
+            response = DiscordScraper.requestData(search_api_url, scraper.headers)
             # Read the response data.
             data2 = loads(response.read().decode('iso-8859-1'))
+
             # ----------------------------------------------------
             grab_text_message_content(data2)
             # Append the messages from data2 into data.
             for message in data2['messages']:
                 data['messages'].append(message)
 
-        # Cache the JSON data if there's anything to cache (don't fill the cache directory with useless API response junk).
+        # Cache the JSON data if there's anything to cache
+        # (don't fill the cache directory with useless API response junk).
         if posts > 0:
             scraper.downloadJSON(data, day.year, day.month, day.day)
-
         # Check the mimetypes of the embedded and attached files.
         scraper.checkMimetypes(data)
 
     # ----------------------------------------------------
     # Set the day to yesterday.
     day += timedelta(days=-1)
-
     # Return the new day
     return day
 
@@ -175,22 +166,21 @@ def start(scraper, guild, channel, day=None):
     :param channel: The ID for the channel that we're wanting to scrape from.
     """
     # Determine if we've already initialized the DiscordScraper class.
-    # If so then clean it out and re-initialize a new one.
+    # If so re-initialize a new one.
     if scraper is not None:
-        del scraper
         scraper = DiscordScraper()
 
     # Determine if the day is empty, default to the current day if so.
     if day is None:
         day = datetime.today()
 
-    # Determine if the year is no less than 2015 since any time before this point will be guaranteed invalid.
-    if day.year <= 2014:
-        exit(0)
-
     # The smallest snowflake that Discord recognizes is from January 1, 2015.
-    while day > datetime(2015, 1, 1):
+    while day >= datetime(2015, 1, 1):
         day = startGuild(scraper, guild, channel, day)
+
+    if day.year <= 2014:
+        print("Requested date is from 2014 or older and is unavailable. Program will now exit.")
+        exit(0)
 
 
 # ----------------------------------------------------------------------------------------------------------------------
